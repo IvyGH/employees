da82f1f698bb79b0d737eee55f915450ed1cad13
root <root@DESKTOP-17CC21M.localdomain>
2020-04-26 01:57:40 +0300

    Registred models

--- employees/list/admin.py
+++ employees/list/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Employee
+
+
+@admin.register(Employee)
+class EmployeeModelAdmin(admin.ModelAdmin):
+    """ Employee model admin """
+    list_display = ('name', 'department', 'position',
+                    'started_work', 'left_work', 'date_of_birth', 'phone_number', 'email', 'github_link', 'slug')
